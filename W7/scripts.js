279b426071803254d2e5fa70c4c247b3623dd276
Aizat Zharylkassynova <a.b.zharylkassynova@gmail.com>
2023-02-12 18:19:21 +0600

    HW 7

--- W7/scripts.js
+++ W7/scripts.js
@@ -1,6 +1,5 @@
-let postList;
-let commentList;
-const postListContainer = document.getElementById("post-list-container");
+const postListContainer = document.getElementById("post-list");
+const commentListContainer = document.getElementById("comment-list");
 
 const getData = async (url = "", params = {}) => {
   const response = await fetch(url + new URLSearchParams(params), {
@@ -12,24 +11,50 @@ const getData = async (url = "", params = {}) => {
   return response.json();
 };
 
+async function getPostById(postId) {
+  await getData("https://jsonplaceholder.typicode.com/posts?", {
+    id: postId,
+  }).then((list) => {
+    const post = list[0];
+    const postTitleElement = document.getElementById("post-title");
+    const postBodyElement = document.getElementById("post-body");
+
+    postTitleElement.innerText = post.title;
+    postBodyElement.innerText = post.body;
+
+    getCommentByPostId(post.id);
+  });
+}
+
 async function getPostList() {
   await getData("https://jsonplaceholder.typicode.com/posts").then((list) => {
-    postList = list ?? [];
-    for (let i = 0; i < postList.length; i++) {
-      console.log("post: ", postList[i]);
-      const postCard = createPostCard(postList[i]);
-      postListContainer.appendChild(postCard);
+    if (list == null || list.length == 0) {
+      postListContainer.innerHTML = `<p>Пусто</p>`;
+      return;
+    }
+    postListContainer.innerHTML = ``;
 
-      addClickListenerToPostCard(postList[i]);
+    for (let i = 0; i < list.length; i++) {
+      const postCard = createPostCard(list[i]);
+      postListContainer.appendChild(postCard);
     }
   });
 }
 
 async function getCommentByPostId(postId) {
-  await getData("https://jsonplaceholder.typicode.com/comments", {
+  await getData("https://jsonplaceholder.typicode.com/comments?", {
     postId,
   }).then((list) => {
-    commentList = list ?? [];
+    if (list == null || list.length == 0) {
+      commentListContainer.innerHTML = `<p>Нет комментарии</p>`;
+      return;
+    }
+    commentListContainer.innerHTML = `<h3>Комментарии</h3>`;
+
+    for (let i = 0; i < list.length; i++) {
+      const postCard = createCommentCard(list[i]);
+      commentListContainer.appendChild(postCard);
+    }
   });
 }
 
@@ -40,24 +65,28 @@ function createPostCard(post) {
                             <div class="card-content">
                                 <h2>${post?.title}</h2>
                                 <p>${post?.body.slice(0, 97).concat("...")}</p>
-                                <a id="post-${
+                                <a class="button" href="./post-detail.html?postId=${
                                   post.id
-                                }" class="button" href="#">Читать дальше</a>
+                                }">Читать дальше</a>
                             </div>
                           </div>`;
 
   return divElement;
 }
 
-function addClickListenerToPostCard(post) {
-  const cardButton = document.getElementById("post-" + post.id);
-  cardButton.addEventListener("click", () => {
-    openPost(post);
+function createCommentCard(comment) {
+  const divElement = document.createElement("div");
+  divElement.className = "comment";
+  divElement.innerHTML = `<h3>${comment.name}</h3>
+                          <h4>${comment.email}</h4>
+                          <p>${comment.body}</p>`;
+
+  return divElement;
+}
+
+function getPostDetails() {
+  const params = new Proxy(new URLSearchParams(window.location.search), {
+    get: (searchParams, prop) => searchParams.get(prop),
   });
+  getPostById(params["postId"]);
 }
-
-function openPost(post) {
-  console.log(post);
-}
-
-getPostList();
